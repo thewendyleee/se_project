27525f16e24f1e840a46e8f45c249a829be76abe
Oscar <h194420kk2@gmail.com>
2022-12-06 12:56:41 +0800

    串接views 和 urls

--- car_rent/views.py
+++ car_rent/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 育慈 
 def rent(request):
     return render(request, "rent.html")
 
 
 def report(request):
     return render(request, "report.html")
+
+# 欣瑩
+def transaction(request):
+    return render(request,"transaction.html")
+
+def transaction_detail(request):
+    return render(request,"transaction_detail.html")
+
+# 世界
+def order(request):
+    return render(request,"order.html")
+
+def return_car(request):
+    return render(request,"return.html")
+
+#佳辰 
+def personal_info(request):
+    return render(request,"personal_info_v2.html")
+
+def personal_info_update(request):
+    return render(request,"personal_info_v2.html")
