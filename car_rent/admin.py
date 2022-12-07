68fa190f1240afc81041932d402a86ba66305e30
HsienHao <kenils0423@gmail.com>
2022-12-07 18:09:43 +0800

    model-2

--- car_rent/admin.py
+++ car_rent/admin.py
@@ -1,5 +1,46 @@
 from django.contrib import admin
-from .models import User
+from .models import *
+
+
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'id', 'account')
+
+
+admin.site.register(User, UserAdmin)
+
+
+class CarTypeAdmin(admin.ModelAdmin):
+    list_display = ('type_name', 'type_number')
+
+
+admin.site.register(CarType, CarTypeAdmin)
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'manufacturer', 'car_type')
+
+
+admin.site.register(Car, CarAdmin)
+
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('station_name', 'address', 'maximum_load')
+
+
+admin.site.register(Station, StationAdmin)
+
+
+# class UserOrderAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'user', 'car', 'station')
+
+
+admin.site.register(UserOrder)
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reason', 'date')
+
+
+admin.site.register(Report, ReportAdmin)
--- car_rent/models.py
+++ car_rent/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.urls import reverse
 import uuid
-# Create your models here.
-class User (models.Model):
 
+
+# Create your models here.
+class User(models.Model):
     user_name = models.CharField(max_length=20)
     account = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
@@ -19,5 +21,84 @@ class User (models.Model):
         choices=SEX,
         default='m',
     )
-    telephone = models.IntegerField()
+    telephone = models.CharField(max_length=10)
     address = models.CharField(max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.user_name
+
+
+class CarType(models.Model):
+    type_name = models.CharField(max_length=10)
+    type_number = models.IntegerField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.type_name
+
+
+class Car(models.Model):
+    st = (
+        ('n', '正常'),
+        ('r', '已預訂'),
+        ('l', '借出中'),
+        ('m', '維修中'),
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=st,
+        default='n',
+    )
+    production_time = models.DateField()
+    manufacturer = models.CharField(max_length=20)
+    Insurance_id = models.CharField(max_length=50)
+    car_type = models.ForeignKey('CarType', on_delete=models.SET_NULL, null=True)
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('car-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.id)
+
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    maximum_load = models.IntegerField(blank=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.station_name
+
+
+class UserOrder(models.Model):
+    order_time = models.DateTimeField()
+    station = models.ForeignKey('Station', on_delete=models.SET_NULL, null=True)  # 站點資訊
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)  # 使用者
+    car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)  # 車輛資訊
+
+    st = (
+        ('p', '未付款'),
+        ('np', '已付款'),
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=st,
+        default='np',
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.id)
+
+
+class Report(models.Model):
+    reason = models.TextField(max_length=1000)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.id)
