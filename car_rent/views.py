0a0fd308ff9e54f47f809a3699c06ad696699e91
Su-edward <55118093+Su-edward@users.noreply.github.com>
2022-12-19 14:51:58 +0800

    Update views.py

--- car_rent/views.py
+++ car_rent/views.py
@@ -26,7 +26,6 @@ def rent(request):
         else:
             Place = None
             CarT = None
-
         #查看顯示剩餘車輛
         # 所有車、所有車數量
         AllCar = Car.objects.all()
@@ -34,51 +33,21 @@ def rent(request):
         # 站點可使用
         bike = 0;
         motorcycle = 0;
-        bikeN = 999999;
-        motorcycleN = 999999;
         #從0開始
         for i in range(AllCarN):
             if (str(AllCar[i].locate_station) == Place and str(AllCar[i].status) == "正常"):
                 if (str(AllCar[i].car_type) == "Bike"):
-                    bikeN = i;
                     bike = bike + 1;
                 if (str(AllCar[i].car_type) == "motorcycle"):
-                    motorcycleN = i;
                     motorcycle = motorcycle + 1
 
+        rent_context['Place'] = Place
+        rent_context['CarT'] = CarT
         rent_context['num_bike'] = bike
         rent_context['num_scooter'] = motorcycle
         rent_context['user_name'] = request.session.get('user_name')
-
-         # 預定車更新CarList
-        if CarT == "腳踏車":
-            if bikeN == 999999:
-                messages.success(request, str(Place) + "已經沒有腳踏車了")
-                return render(request, "rent.html", rent_context)
-            else:
-                C = AllCar[bikeN]
-                C.status = '已預訂'
-                C.save()
-        if CarT == "電動滑板車":
-            if motorcycleN == 999999:
-                messages.success(request, str(Place) + "已經沒有電動滑板車了")
-                return render(request, "rent.html", rent_context)
-            else:
-                C = AllCar[motorcycleN]
-                C.status = '已預訂'
-                C.save()
-
-        #建立order物件
-        if Place != None and CarT!= None:
-            U = User.objects.get(id=user_id)
-            S = Station.objects.get(station_name=Place)
-            date1=datetime.now()
-            items = Order.objects.create( order_station =S, order_time=date1,order_user =U,order_car=C)
-            items.save()
-            messages.success(request, "預約成功")
-            return redirect('/order/')
-
         return render(request, "rent.html", rent_context)
+
 
 
 def report(request):
